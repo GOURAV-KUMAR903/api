45fbcdc02620257411d3c9d9aa931533ee9e6052
GOURAV-KUMAR903 <gni.gourav@gmail.com>
2026-06-03 14:13:14 +0530

    some change dfa

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
-$request = $_SERVER['REQUEST_URI'];
+header("Content-Type: application/json");
 
-if ($request == "/posts") {
-    include "api/posts.php";
-} else {
-    echo "API Running";
-}
+require_once "config/database.php";
+require_once "models/Post.php";
+
+$db = new Database();
+$conn = $db->connect();
+
+$post = new Post($conn);
+
+$data = $post->getAll();
+
+echo json_encode([
+    "status" => true,
+    "count" => count($data),
+    "data" => $data
+]);
