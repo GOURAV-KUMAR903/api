5e2875b0c9ad5681fb6b0cb24b48c16fe074b446
GOURAV-KUMAR903 <gni.gourav@gmail.com>
2026-06-04 11:27:03 +0530

    one again one again

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 class Database {
 
-    private $host = "159.89.36.188";
+    private $host = "https://gnitaskmanagement.com/";
     private $port = "3306";
     private $db_name = "gnitaskm_test";
     private $username = "gnitaskm";
